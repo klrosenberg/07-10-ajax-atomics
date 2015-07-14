98743818063f9debd752604c7111769e89317dc0
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-13 22:01:24 -0500

    Get href attribute

--- 03/public/global.js
+++ 03/public/global.js
@@ -4,7 +4,7 @@ var myLink = document.getElementById("clicker");
 var handleTheClick = function(event){
   var req = new XMLHttpRequest();
   
-  req.open("get", "/path1");
+  req.open("get", this.getAttribute("href"));
 
   req.addEventListener("load", function(){
     alert(this.response);
