016574f0be6f3a656ab0eb42767b203ee9dcc10b
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-13 21:22:19 -0500

    Complete 01-part 3. Add if statement.

--- 01/public/global.js
+++ 01/public/global.js
@@ -8,7 +8,11 @@ req.addEventListener("loadstart", function(){
 });
 
 req.addEventListener("load", function(){
-  alert(this.response);
+  if (this.response === "yes") {
+      alert("Yay!")
+    } else {
+      alert("Aw, shucks!")
+  }
 });
 
 req.send();
